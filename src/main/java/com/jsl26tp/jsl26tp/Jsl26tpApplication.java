83f485e08e4fa06973dc53d6b3cb2bb3f39d57f6
thisisminseon <parkms9706@gmail.com>
2026-03-02 19:24:39 +0900

    init: Spring Boot Project Initial Settings

---
+++ src/main/java/com/jsl26tp/jsl26tp/Jsl26tpApplication.java
@@ -0,0 +1,13 @@
+package com.jsl26tp.jsl26tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jsl26tpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jsl26tpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsl26tp/jsl26tp/Jsl26tpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsl26tp.jsl26tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jsl26tpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
